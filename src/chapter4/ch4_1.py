3461365c41261b0729f00bcb6a1b4864b8887bfd
sekichang <sekichang@gmail.com>
2019-05-26 18:08:16 +0900

    first commit

---
+++ src/chapter4/ch4_1.py
@@ -0,0 +1,6 @@
+def squared(x):
+    return x ** 2
+
+x = int(input("数値xを入力して下さい。="))
+
+print("数値xの2乗は{}です".format(squared(x)))
---
+++ src/chapter4/ch4_2.py
@@ -0,0 +1,6 @@
+def print_message(msg):
+    print(msg)
+
+msg = input("メッセージを入力して下さい。:")
+
+print_message(msg)
